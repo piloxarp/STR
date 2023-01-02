404423cf6198c873d7b43a1da2d77c1b0ed24e03
piloxarp <piloxarp@gmail.com>
2023-01-02 21:12:10 +0300

    1.4b

---
+++ projects/My Girlfriend's Abroad/system/setup.js
@@ -0,0 +1,18 @@
+/*
+*
+* You may install games here manually, however this can be done for you when you export games using CGM Editor.
+* Add games .js filenames here (without the "//" in front)
+* For example:
+* var addGamesHere = ['My Game.js','My Other Game.js'];
+*
+*/
+var addGamesHere = ['my-girlfriends-abroad.js'];
+
+/*
+*
+* For internal use only
+*
+* Do not edit below this line
+*
+*/
+var i,sizeOf = addGamesHere.length;for (i=0;i!=sizeOf;i++){document.write("<script type='text/javascript' src='games/"+addGamesHere[i]+"'></script>");console.log("Installing game at path: 'games/"+addGamesHere[i]+"'");}window.setup_1=false,window.setup_2=true,window.setup_3=true,window.setup_4=true,window.setup_5=true,window.setup_6=false,window.setup_7=false,window.setup_8=false,window.setup_9=true,window.setup_10=false,window.setup_11=true,window.setup_12=true,window.setup_13=false,window.setup_14=false,window.setup_15=false,window.setup_16=false,window.setup_17=false,window.setup_18=false,window.setup_19=true,window.setup_20=false,window.setup_21=true,window.setup_22=false,window.setup_23=true,window.setup_24=false,window.setup_25=true,window.setup_26=true,window.setup_27=true,window.setup_28=true,window.setup_29=false,window.setup_30=false,window.setup_31=true,window.setup_32=false,window.setup_33=false,window.setup_34=false,window.setup_35=false,window.setup_36=false,window.setup_37=true,window.setup_38=true,window.setup_39=true,window.setup_40=true,window.setup_41=false,window.setup_42=false,window.setup_43=false,window.setup_44=true,window.setup_45=false,window.setup_46=false,window.setup_47=true,window.setup_48=true,window.setup_49=true,window.setup_50=false,window.setup_51=false,window.setup_52=true,window.setup_53=false,window.setup_54=true,window.setup_55=true,window.setup_56=true,window.setup_57=true,window.setup_58=false,window.setup_59=false,window.setup_60=false,window.setup_61=true,window.setup_62=true,window.setup_63=false,window.setup_64=false,window.setup_65=true,window.setup_66=false,window.setup_67=false;console.log('Setup Complete!');
